c9001d8edad2f61ef2f0d91c9158dec18faf1db1
Pirat102 <patryk.pirowski@gmail.com>
2024-10-14 16:49:59 +0200

    readme etc, preparation for finishing the project

--- Job_scraper/test.py
+++
@@ -1,8 +0,0 @@
-from project import *
-
-with open("test_offers.html", "r", encoding="UTF8") as f:
-        html = f.read()
-
-offers = get_each_job_title_link(html)
-print(offers)
-print(len(offers))
